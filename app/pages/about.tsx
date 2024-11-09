5c8c5634c319a0ec335155716104968078693f67
Sarah Stec <sastec@umich.edu>
2024-11-09 09:25:58 -0500

    start on navbar - todo: separate pages

--- app/pages/about.tsx
+++ app/pages/about.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image'
 import Link from 'next/link'
 
-export default function Home() {
+export default function About() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <p>Home</p>
+        <p>About</p>
     </main>
   )
 }
