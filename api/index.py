73c1b9b1d0ab4df8127bf0738c7b45247ca6c8dd
Sarah Stec <sastec@umich.edu>
2024-11-10 09:01:55 -0500

    Pull data from BE

--- api/index.py
+++ api/index.py
@@ -4,3 +4,19 @@ app = Flask(__name__)
 @app.route("/api/python")
 def hello_world():
     return "<p>Hello, World!</p>"
+
+@app.route("/api/completeTheMeasure")
+def completeTheMeasure():
+    # TODO - SET UP SQLITE DATABASE + PULL IMAGE WE HAVEN'T SEEN BEFORE
+    # STORE ARRAY OF IMAGES SEEN BEFORE IN REQUEST.SESSION? AVOID REPEATS IF POSSIBLE?
+    # TODO: RETURN DESCRIPTION/ correct answer OF EXERCISE (in DB)- Use for future API call 
+
+    # IF user gets answer correct, move onto next exercise + call this function again
+    # ELSE - If user gets wrong - Sep API call that feeds this info into OpenAI + requests hint
+    return {"imgUrl":  "/app/images/half_note.png"}
+
+
+@app.route("/api/getOpenAIFeedback")
+def getOpenAIFeedback():
+    """Get feedback from openAI - See above"""
+    return ""
--- app/exercises/completeMeasure/page.tsx
+++ app/exercises/completeMeasure/page.tsx
@@ -1,17 +1,19 @@
 /* completeMeasure component
 */
+"use client";
 import MultipleChoice from "@/app/components/multipleChoice"
 
+
+
 export default function completeMeasure() {
+
     return (
       <main className="flex min-h-screen flex-col items-center pt-24">
          <h1 className="text-3xl font-semibold mb-5">Complete the Measure</h1>
          <p className="w-1/2 text-center">Given the following <b>incomplete</b> measure, select the note length 
          that should replace the question mark to complete the measure.
          </p>
-         
-         <p> TODO - Load image from BE</p>
-         <MultipleChoice></MultipleChoice>
+         <MultipleChoice url="/api/completeTheMeasure"></MultipleChoice>
       </main>
     )
   }
