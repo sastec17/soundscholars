c7c6a18f2e1ac951cf301568fc267ce3cdd47f17
Sarah Stec <sastec@umich.edu>
2024-11-16 09:54:40 -0500

    clean up main exercises page

--- app/exercises/page.tsx
+++ app/exercises/page.tsx
@@ -1,44 +1,66 @@
 import Link from "next/link"
 export default function Exercises() {
     return(
-        <main className="flex min-h-screen flex-col items-center pt-24">
-        
-        
-        <h1 className="text-3xl font-semibold mb-5">Exercises</h1>
-        <p>Select an activity and begin learning :p</p>
-        <p> Not sure if this page is needed?? </p>
+        <main className="flex min-h-screen flex-col items-center">
+            <nav className="sticky top-0 left-0 z-50 w-full text-black py-4">
+                <div className="md:px-8">
+                {/** Top NavBar */}
+                <section className="flex justify-between items-center">
+                    <Link href='/exercises' className="text-xl font-bold">
+                        SoundScholars
+                    </Link>
+                    <div className="space-x-8">
+                        <Link href="/about" className="text-lg font-semibold hover:underline">
+                            About
+                        </Link>
+                        <Link href="/learningPages" className="text-lg font-semibold hover:underline">
+                            Learning Pages
+                        </Link>
+                        <Link href="/logout" className="text-lg font-semibold hover:underline">
+                            Logout
+                        </Link>
+                    </div>
+                </section>
+            </div>
+        </nav>
+        <div className="flex flex-grow flex-col pt-24 text-center">
+            <h1 className="text-3xl font-semibold mb-5">Exercises</h1>
+            <p>Select an activity and begin learning :p</p>
+            <p> Not sure if this page is needed?? </p>
 
-        <p>TODO: Styling - make all the boxes same size, regardless of text</p>
-        <div className='flex flex-col space-y-10'>
-            <div className="flex items-center space-x-10">
-                <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
-                    <Link href='/exercises/noteAddition'>
-                        <p className="font-semibold text-xl">Note Addition</p>
-                        <p className="text-md">Identify equivalent note values</p>
-                    </Link>
+            <p>TODO: Styling - make all the boxes same size, regardless of text</p>
+            <div className='flex flex-col space-y-10'>
+                <div className="flex items-center space-x-10">
+                    <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
+                        <Link href='/exercises/noteAddition'>
+                            <p className="font-semibold text-xl">Note Addition</p>
+                            <p className="text-md">Identify equivalent note values</p>
+                        </Link>
+                    </div>
+                    <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
+                        <Link href='/exercises/notationIdentification'>
+                            <p className="font-semibold text-xl">Notation Identification</p>
+                            <p className="text-md">Identify notes and rests</p>
+                        </Link>
+                    </div>
                 </div>
-                <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
-                    <Link href='/exercises/notationIdentification'>
-                        <p className="font-semibold text-xl">Notation Identification</p>
-                        <p className="text-md">Identify notes and rests</p>
-                    </Link>
+
+                <div className="flex items-center space-x-10">
+                    <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
+                        <Link href='/exercises/noteAddition'>
+                            <p className="font-semibold text-xl">Complete the Measure</p>
+                            <p className="text-md">Uncover the missing note</p>
+                        </Link>
+                    </div>
+                    <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
+                        <Link href='/exercises/notationIdentification'>
+                            <p className="font-semibold text-xl">Type that Rhythm</p>
+                            <p className="text-md">Apply notation and time signature knowledge</p>
+                        </Link>
+                    </div>
                 </div>
             </div>
 
-            <div className="flex items-center space-x-10">
-                <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
-                    <Link href='/exercises/noteAddition'>
-                        <p className="font-semibold text-xl">Complete the Measure</p>
-                        <p className="text-md">Uncover the missing note</p>
-                    </Link>
-                </div>
-                <div className='py-4 px-3 bg-indigo-300 rounded-md text-center'>
-                    <Link href='/exercises/notationIdentification'>
-                        <p className="font-semibold text-xl">Type that Rhythm</p>
-                        <p className="text-md">Apply notation and time signature knowledge</p>
-                    </Link>
-                </div>
-            </div>
         </div>
      </main>
     )
