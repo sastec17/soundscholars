2702007eb3a4d9ffb4d4edd05e48435955de33e4
Sarah Stec <sastec@umich.edu>
2024-11-17 14:48:58 -0500

    add welcome page

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -2,14 +2,19 @@
 import Link from "next/link"
 export default function Welcome() {
     return (
-        <main className="flex min-h-screen flex-col items-center justify-between p-24">
-            <p>TODO: Implement welcome splash screen - See Figma :p</p>
-
-            {/** TODO: Link to learning page 0 */}
-            <Link href='/learningPages/levels/level00' className="bg-indigo-300 hover:bg-indigo-500 text-black font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline" type="button">
-                Get Started
-            </Link>
-    
-        </main>
+    <main className="flex min-h-screen flex-col items-center">
+        <div className="flex flex-col flex-grow items-center justify-center w-full space-y-10">
+          <h1 className="flex items-center text-5xl font-bold justify-center m-4">Welcome to SoundScholars!</h1>
+          <p className='text-3xl font-semibold'>The premier tutor for learning musical rhythms</p>
+          <div className="text-center text-lg">
+            <p>Your goal is to learn rhythms and "level up" by completing exercises.</p>
+            <p>We will introduce new concepts via learning pages after you complete a level.</p>
+          </div>
+          <div className='flex justify-center m-5 space-x-6'>
+            <Link className='p-5 bg-indigo-300 font-semibold rounded-lg'
+              href='/learningPages/levels/level00'>Get Started</Link>
+          </div>
+        </div>
+    </main>
       )
 }
